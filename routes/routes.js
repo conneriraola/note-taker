88a7724ae7ea450b13a25a8b0879a5d6075789e6
Conner Iraola <conner_iraola@yahoo.com>
2022-05-07 14:46:54 -0700

    updates to routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,23 +3,70 @@ const path = require('path');
 
 module.exports = app => {
 
-    fs.readFile('db/db.json', 'utf8', (err, data) => {
-
-        if (err) throw err;
-
-        var notes = JSON.parse(data);
-
 
         app.get('/api/notes', function(req, res) {
-            res.json(notes);
+            
+            fs.readFile('db/db.json', 'utf8', (err, data) => {
+
+                if (err) throw err;
+                var notes = JSON.parse(data);
+                res.json(notes);
+            });
         });
 
-        app.post('/api/notes', function(req,res) {
-            let newNote = req.body;
-            notes.push(newNote);
-            updateDb();
-            return console.log('Added new note');
-        });
+        // app.post('/api/notes', function(req,res) {
+        //     let newNote = req.body;
+        //     notes.push(newNote);
+        //     updateDb();
+        //     return console.log('Added new note');
+        // });
+
+        // POST request to add a review
+        app.post('/api/notes', (req, res) => {
+            // Log that a POST request was received
+            console.info(`${req.method} request received to add a review`);
+  
+            // Destructuring assignment for the items in req.body
+            const { title, text } = req.body;
+  
+            // If all the required properties are present
+            if (title && text) {
+                // Variable for the object we will save
+                const newNote = {
+                    title,
+                    text,
+                    note_id: uuid(),
+                };
+  
+                // Obtain existing reviews
+                fs.readFile('./db/db.json', 'utf8', (err, data) => {
+                    if (err) {
+                    console.error(err);
+                    } else {
+                        // Convert string into JSON object
+                        const parsedNotes = JSON.parse(data);
+        
+                        // Add a new review
+                        parsedNotes.push(newNote);
+        
+                        // Write updated reviews back to the file
+                        fs.writeFile(
+                            './db/db.json',
+                            JSON.stringify(parsedNotes, null, 4),
+                            (writeErr) =>
+                            writeErr
+                                ? console.error(writeErr)
+                                : console.info('Successfully updated notes!')
+                        );
+                    }
+                });
+                const response = {
+                 status: 'success',
+                 body: newNote,
+                };
+                res.json(response)
+            }
+        })
 
         app.get('/api/notes/:id', function(req,res) {
             res.json(notes[req.params.id]);
@@ -34,8 +81,8 @@ module.exports = app => {
         });
 
 
-        app.get('*', function(req,res) {
-            res.sendFile(path.join(__dirname, "../public/index.html"));
+        app.get('/', function(req,res) {
+            res.sendFile(path.join(__dirname, "./public/index.html"));
         });
 
         function updateDb() {
@@ -44,5 +91,4 @@ module.exports = app => {
                 return true;
             });
         }
-    });
 }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+const routes = require('./routes/routes')
 const PORT = process.env.PORT||3000;
 const app = express();
 
@@ -9,7 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-require('./routes/routes')(app);
+// require('./routes/routes')(app);
+
+app.use(routes)
 
 app.listen(PORT, function() {
   console.log(`App listening at http://localhost:${PORT} 🚀`);
